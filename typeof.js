911882270a9342b157964187408d0e70ca5cb65d
Edwin Vasquez <edwin.vasquez@grupopdc.com>
2021-06-03 21:09:16 -0600

    typeof

---
+++ typeof.js
@@ -0,0 +1,10 @@
+//Acá simplemente diremos que la función typeof sirve para saber el tipo de valor que nos están enviando y retornarlo
+
+typeof(1);                  //number
+typeof(true);               //boolean
+typeof('hello');            //string
+typeof( function(){} );     //function
+typeof({});                 //object
+typeof(null);               //object
+typeof(undefined);          //undefined
+typeof(NaN);                //number
